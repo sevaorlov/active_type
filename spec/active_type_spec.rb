235902478ecfab63aebfec33fd4b1d69e950fa3f
Vsevolod <vsevolod@ubuntu.(none)>
2013-02-18 21:37:39 +0400

    add negative test

--- spec/active_type_spec.rb
+++ spec/active_type_spec.rb
@@ -5,18 +5,22 @@ describe "ActiveType" do
   it "serialize a type" do
     ActiveType.add_properties [:pr1, :pr2, :pr3] 
     type = ActiveType.new
-    type[:pr1] = "pr1_text"
-    type[:pr2] = "pr2_text"
-    type[:pr3] = "pr3_text"
+    type.pr1 = "pr1_text"
+    type.pr2 = "pr2_text"
+    type.pr3 = "pr3_text"
     ActiveType.dump(type).should eql('("pr1_text","pr2_text","pr3_text")')
   end
 
   it "deserialize a type" do
     ActiveType.add_properties [:one, :two, :three]
     type = ActiveType.load("(moskow,mohovaya,28)")
-    type[:one].should eql("moskow") 
-    type[:two].should eql("mohovaya")
-    type[:three].should eql("28")
+    type.one.should eql("moskow") 
+    type.two.should eql("mohovaya")
+    type.three.should eql("28")
   end
 
+  it "raise error when properties not equal on deserialization" do 
+    ActiveType.add_properties [:one, :two, :three]
+    expect{ActiveType.load("(moskow,mohovaya,28,sometext)")}.to raise_error
+  end
 end
