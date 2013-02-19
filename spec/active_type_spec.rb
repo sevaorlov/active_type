a57f55d05ce9388a001c5361b1bf528de714df9c
Vsevolod <vsevolod@ubuntu.(none)>
2013-02-19 21:45:33 +0400

    change tests

--- spec/active_type_spec.rb
+++ spec/active_type_spec.rb
@@ -1,54 +1,67 @@
-require 'active_type' 
+require 'active_type/active_type' 
+require 'active_record'
 
 describe "ActiveType" do
-  class Address < ActiveType
-     property :city
-     property :street
-     property :postal_code
-  end
-  
-  
-  class Person < ActiveRecord::Base
-     serialize Address
-  end
-  
-  let(:connection_hash) { {database: 'test-active-type'} }
 
-  before(:all) do
-     ActiveRecord::Base.establish_connection(connection_hash)
-     ActiveRecord::Base.connection.execute <<-SQL
-        DROP TYPE IF EXISTS Address;
-        DROP TABLE IF EXISTS people;
-        CREATE TYPE Address (city varchar, street varchar, zip varchar);
+  describe "with db" do 
+    class Address < ActiveType
+       properties :city, :street, :postal_code
+    end
+        
+    class Person < ActiveRecord::Base
+       serialize :address, Address
+       attr_accessor :name
+    end
+      
+    let(:connection_hash) { { 
+      adapter: 'postgresql',
+      database: 'test-active-type',
+      host: 'localhost',
+      username: 'postgres',
+      password: 'qwe324012',        
+    } }
+
+    before(:all) do
+      ActiveRecord::Base.establish_connection(connection_hash)
+      ActiveRecord::Base.connection.execute <<-SQL
+	DROP TYPE IF EXISTS Address CASCADE;
+        DROP TABLE IF EXISTS people CASCADE;
+        CREATE TYPE Address AS (city varchar, street varchar, zip varchar);
         CREATE TABLE people ( name varchar, address Address);
-     SQL
-  end
-  
-  it "should work :)" do
-    person = Person.create!( name: 'Ivan Groznij', address: Address.new( city: 'Moscow'))
-    person.reload
-    person.address.city.should == 'Moscow'
-  end
-  
-  it "serialize a type" do
-    ActiveType::TypeClass.add_properties [:pr1, :pr2, :pr3] 
-    type = ActiveType::TypeClass.new
-    type.pr1 = "pr1_text"
-    type.pr2 = "pr2_text"
-    type.pr3 = "pr3_text"
-    ActiveType::TypeClass.dump(type).should eql('("pr1_text","pr2_text","pr3_text")')
+      SQL
+    end
+      
+    it "should work :)" do
+      person = Person.create!( name: 'Ivan Groznij', address: Address.new( city: 'Moscow'))
+      person.reload
+      person.address.city.should == 'Moscow'
+    end
   end
 
-  it "deserialize a type" do
-    ActiveType::TypeClass.add_properties [:one, :two, :three]
-    type = ActiveType::TypeClass.load("(moskow,mohovaya,28)")
-    type.one.should eql("moskow") 
-    type.two.should eql("mohovaya")
-    type.three.should eql("28")
-  end
-
-  it "raise error when properties not equal on deserialization" do 
-    ActiveType::TypeClass.add_properties [:one, :two, :three]
-    expect{ActiveType::TypeClass.load("(moskow,mohovaya,28,sometext)")}.to raise_error
-  end
+  describe "serialization" do
+  
+    class SomeType < ActiveType
+      properties :pr1, :pr2, :pr3
+    end
+    
+    it "serialize a type" do             
+      type = SomeType.new
+      type.pr1 = 'pr1_text'
+      type.pr2 = 'pr2_text'
+      type.pr3 = 'pr3_text'
+      SomeType.dump(type).should eql('("pr1_text","pr2_text","pr3_text")')
+    end
+          
+    it "deserialize a type" do         
+      type = SomeType.load("(moskow,mohovaya,28)")
+      type.pr1.should eql("moskow") 
+      type.pr2.should eql("mohovaya")
+      type.pr3.should eql("28")
+    end
+          
+    it "raise error when properties not equal on deserialization" do          
+      expect{SomeType.load("(moskow,mohovaya,28,sometext)")}.to raise_error
+    end
+  end  
+  
 end
