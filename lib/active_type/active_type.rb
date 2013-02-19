0d5dbe740c45704b903c282ce0f744f7ce6367f0
Vsevolod <vsevolod@ubuntu.(none)>
2013-02-19 21:46:37 +0400

    move active_type and make it a class

---
+++ lib/active_type/active_type.rb
@@ -0,0 +1,38 @@
+class ActiveType
+  @@properties = []
+
+  def self.load str
+    vals = str[1, str.length-2].split(",")
+    raise "Wrong type attributes!" if vals.length != @@properties.length
+
+    i = 0
+    type = self.new
+    @@properties.each do |property|
+      type.send "#{property}=", vals[i]
+      i += 1
+    end
+    type
+  end
+
+  def self.dump type
+    str = '('
+    first = true
+    type.instance_variables.each do |property|          
+      if !first
+	str << ','
+      else
+        first = false
+      end
+      str << "\"#{type.instance_variable_get(property)}\""
+    end
+    str << ')'      
+  end
+  
+  def self.properties *params
+    @@properties = []   
+    params.each do |property|
+      class_eval { attr_accessor property}
+      @@properties << property
+    end
+  end
+end
